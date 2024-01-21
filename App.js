89ac0a97ec7fc6d5012c9416965e1303c5a7b4b2
VoCaoQuoc2004 <vocaoquoc111@gmail.com>
2024-01-21 16:39:08 +0700

    lab3

--- App.js
+++ App.js
@@ -5,29 +5,31 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignIn from "./src/screens/auth/SignIn";
 import SignUp from "./src/screens/auth/SignUp";
-
+import ViewComponent from "./src/screens/lab3/bai1_2";
+import ModalComponent from "./src/screens/lab3/bai3";
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{
-          headerTitleStyle: {
-            color: 'blue', // Đặt màu chữ ở đây
-          },
-        }}name="Home" component={Splash}/>
-        <Stack.Screen options={{
-          headerTitleStyle: {
-            color: 'blue', // Đặt màu chữ ở đây
-          },
-        }} name="SignIn" component={SignIn}/>
-        <Stack.Screen  options={{
-          headerTitleStyle: {
-            color: 'blue', // Đặt màu chữ ở đây
-          },
-        }} name="SignUp" component={SignUp}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen options={{
+    //       headerTitleStyle: {
+    //         color: 'blue', // Đặt màu chữ ở đây
+    //       },
+    //     }}name="Home" component={Splash}/>
+    //     <Stack.Screen options={{
+    //       headerTitleStyle: {
+    //         color: 'blue', // Đặt màu chữ ở đây
+    //       },
+    //     }} name="SignIn" component={SignIn}/>
+    //     <Stack.Screen  options={{
+    //       headerTitleStyle: {
+    //         color: 'blue', // Đặt màu chữ ở đây
+    //       },
+    //     }} name="SignUp" component={SignUp}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <ModalComponent/>
   );
 };
 export default App;
--- src/screens/lab3/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-
-const Mylab3 = () => {
-  return <Text>Day la lab3</Text>;
-};
-  export default Mylab3;
